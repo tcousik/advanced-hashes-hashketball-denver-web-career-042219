b60fe17263aa41407027d858160e2248fe3ff1da
Tejas Cousik <tcousik0@gmail.com>
2019-04-25 19:22:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,5 +181,6 @@ def player_stats (player_name)
 end
 
 def big_shoe_rebounds(player_name)
-  big_shoe_player = players.map do 
+  big_shoe_player = players.map do |player_name, player_data|
+    
 end
